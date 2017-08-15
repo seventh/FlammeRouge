476f726c8a506f94f2f2bb7bbb76372ff200714c
Guillaume Lemaître <guillaume.lemaitre@gmail.com>
2017-08-15 12:27:38 +0200

    Création d'un nouveau format TRAJET4

--- util/convertisseur.py
+++ util/convertisseur.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""Convertisseur d'un fichier de recherche du format TRAJET1 à TRAJET3
+"""Convertisseur d'un fichier de recherche du format TRAJET1 à TRAJET4
 
 Un fichier TRAJET1 se décompose ainsi :
   - succession d'entiers sur 6 octets, gros-boutisme
@@ -8,6 +8,10 @@ Un fichier TRAJET1 se décompose ainsi :
 Un fichier TRAJET3 se décompose ainsi :
   - un entier sur 5 octets, gros-boutisme
   - une succession de bits
+
+Un fichier TRAJET4 se décompose ainsi :
+  - une suite de bits, chaque bit représentant l'état "le trajet est
+    constructible" du trajet de code «trajet2» correspondant.
 """
 
 import trajet
@@ -15,7 +19,7 @@ import trajet2
 
 
 ENTRÉE = "trajets.bin"
-SORTIE = "trajets3.bin"
+SORTIE = "trajets4.bin"
 
 
 class Trajet1:
@@ -63,9 +67,34 @@ class Trajet3:
         self._octets[octet] |= 1 << (7 - bit)
 
 
+class Trajet4:
+
+    def __init__(self, chemin):
+        try:
+            self._flux = open(chemin, "r+b")
+        except FileNotFoundError:
+            self._flux = open(chemin, "x+b")
+        self._flux.truncate((trajet2.CODE_GARDE + 7) // 8)
+
+    def __del__(self):
+        self._flux.close()
+        self._flux = None
+
+    def met(self, t):
+        code = trajet2.coder(t)
+        décalage, bit = divmod(code, 8)
+        self._flux.seek(décalage, 0)
+        octet = self._flux.read(1)
+        valeur = int.from_bytes(octet, "little")
+        valeur |= 1 << bit
+        self._flux.seek(-1, 1)
+        octet = valeur.to_bytes(1, "little")
+        self._flux.write(octet)
+
+
 if __name__ == "__main__":
     E = Trajet1(ENTRÉE)
-    S = Trajet3(SORTIE)
+    S = Trajet4(SORTIE)
 
     while True:
         t = E.lit()
